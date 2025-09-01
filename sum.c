974f6f7586c4f96d04b4f340ea07d340098b1820
Avieate <aditverma6819@gmail.com>
2025-09-01 14:50:43 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,15 @@
+//Experiment 1-4 by Adit.27411
+#include <stdio.h>
+
+
+ int main(void)
+
+ {
+    float x, y;
+
+    printf("Input: ");
+    scanf("%f %f", &x, &y);
+     {
+         printf("Sum = %g\n", x + y);
+     }
+}
