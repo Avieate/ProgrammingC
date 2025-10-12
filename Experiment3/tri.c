28b2e2f4d1130ffe92c3b0971099dd7daabc759d
Avieate <aditverma6819@gmail.com>
2025-10-12 18:45:41 +0530

    Fixed a typo.

--- Experiment3/tri.c
+++ Experiment3/tri.c
@@ -5,7 +5,7 @@ int main(void)
     float a, b, c;
 
     printf("Lengths: ");
-    scanf("%f %f %F", &a, &b, &c);
+    scanf("%f %f %f", &a, &b, &c);
 
     if (a + b > c && a + c > b && b + c > a)
     {
